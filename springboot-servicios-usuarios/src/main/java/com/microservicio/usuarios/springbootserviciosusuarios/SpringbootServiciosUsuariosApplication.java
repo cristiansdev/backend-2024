130d32b1e221bd954acc34487871d05e6d45b7f4
cristiansdev <cristian192003@outlook.es>
2024-04-25 23:28:32 -0600

    Se integro el nuevo servicio de usuarios al microservicio

---
+++ springboot-servicios-usuarios/src/main/java/com/microservicio/usuarios/springbootserviciosusuarios/SpringbootServiciosUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.usuarios.springbootserviciosusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiciosUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiciosUsuariosApplication.class, args);
+	}
+
+}
---
+++ springboot-servicios-usuarios/src/test/java/com/microservicio/usuarios/springbootserviciosusuarios/SpringbootServiciosUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.usuarios.springbootserviciosusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiciosUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
